90905da58791e4f6fc87f767271b1d7a9b50d072
Yusuke Nojima <nojima@ynojima.com>
2015-02-03 21:59:58 +0900

    Scalate ではなく Twirl を使うようにした。

--- project/build.scala
+++ project/build.scala
@@ -2,8 +2,7 @@ import sbt._
 import Keys._
 import org.scalatra.sbt._
 import org.scalatra.sbt.PluginKeys._
-import com.mojolly.scalate.ScalatePlugin._
-import ScalateKeys._
+import play.twirl.sbt.SbtTwirl
 
 object CarbonBuild extends Build {
   val Organization = "com.example"
@@ -15,7 +14,7 @@ object CarbonBuild extends Build {
   lazy val project = Project (
     "carbon",
     file("."),
-    settings = ScalatraPlugin.scalatraWithJRebel ++ scalateSettings ++ Seq(
+    settings = ScalatraPlugin.scalatraWithJRebel ++ Seq(
       organization := Organization,
       name := Name,
       version := Version,
@@ -23,25 +22,12 @@ object CarbonBuild extends Build {
       resolvers += Classpaths.typesafeReleases,
       libraryDependencies ++= Seq(
         "org.scalatra" %% "scalatra" % ScalatraVersion,
-        "org.scalatra" %% "scalatra-scalate" % ScalatraVersion,
         "org.scalatra" %% "scalatra-specs2" % ScalatraVersion % "test",
         "ch.qos.logback" % "logback-classic" % "1.1.2" % "runtime",
         "org.eclipse.jetty" % "jetty-webapp" % "9.1.5.v20140505" % "container",
         "org.eclipse.jetty" % "jetty-plus" % "9.1.5.v20140505" % "container",
         "javax.servlet" % "javax.servlet-api" % "3.1.0"
-      ),
-      scalateTemplateConfig in Compile <<= (sourceDirectory in Compile){ base =>
-        Seq(
-          TemplateConfig(
-            base / "webapp" / "WEB-INF" / "templates",
-            Seq.empty,  /* default imports should be added here */
-            Seq(
-              Binding("context", "_root_.org.scalatra.scalate.ScalatraRenderContext", importMembers = true, isImplicit = true)
-            ),  /* add extra bindings here */
-            Some("templates")
-          )
-        )
-      }
+      )
     )
-  )
+  ).enablePlugins(SbtTwirl)
 }
--- src/main/scala/app/CarbonServlet.scala
+++ src/main/scala/app/CarbonServlet.scala
@@ -1,17 +1,20 @@
 package app
 
 import org.scalatra._
-import scalate.ScalateSupport
 
-class CarbonServlet extends CarbonStack {
+class CarbonServlet extends ScalatraServlet {
 
   get("/") {
     <html>
       <body>
         <h1>Hello, world!</h1>
-        Say <a href="hello-scalate">hello to Scalate</a>.
+        Say <a href="/hello-twirl">hello to Twirl</a>.
       </body>
     </html>
+  }
+
+  get("/hello-twirl") {
+    html.helloTwirl.render(new java.util.Date)
   }
 
 }
