360810fe0466b2d58be91c06b994db6aa83e9552
mhkoji <mhkoji@gmail.com>
2015-06-20 23:21:01 +0900

    FolderServiceをリファクタ

--- src/main/scala/services/FolderService.scala
+++ src/main/scala/services/FolderService.scala
@@ -1,26 +1,14 @@
 package services
 
-import java.io.File
-
 import dto.{Folder => FolderDto}
 import logic.folder.{FolderComponent => FolderLogicComponent}
-import scala.slick.driver.H2Driver.simple._
 
-class FolderService(baseDir: String) { this: FolderLogicComponent => 
-  def makeFolderPath(folder: FolderDto): String =
-    s"${baseDir}/${folder.owner}/${folder.name}"
-
+class FolderService { this: FolderLogicComponent =>
   def addFolder(folder: FolderDto): Int = {
-    val folderId = folderLogic.insert(folder)
-
-    val folderPath = makeFolderPath(folder)
-    val folderDirectory = new File(folderPath)
-    if (!folderDirectory.mkdirs()) {
-      folderLogic.delete(folderId);
-      throw new RuntimeException("Failed to create directory: " + folderPath)
+    if (folder.owner.isEmpty || folder.name.isEmpty) {
+      throw new IllegalArgumentException("Could not create folder");
     }
-
-    folderId
+    folderLogic.insert(folder)
   }
 
   def findFolder(folderId: Int): Option[FolderDto] = folderLogic.find(folderId)
