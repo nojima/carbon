9b122aa28219fb5f48a38be7b1f3d46808f3dad7
mhkoji <mhkoji@gmail.com>
2015-06-21 11:19:03 +0900

    pegdownのExtensionsを追加

--- src/main/scala/services/RenderService.scala
+++ src/main/scala/services/RenderService.scala
@@ -1,9 +1,10 @@
 package services
 
+import org.pegdown.Extensions
 import org.pegdown.PegDownProcessor
 
 class RenderService {
-  private val processor = new PegDownProcessor()
+  private val processor = new PegDownProcessor(org.pegdown.Extensions.ALL)
 
   def renderMarkdown(text: String): String = processor.markdownToHtml(text)
 }
