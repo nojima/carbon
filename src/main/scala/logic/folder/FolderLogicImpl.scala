5e492e253644ca22bd42dd1b559f35a537572a53
mhkoji <mhkoji@gmail.com>
2015-06-23 00:21:50 +0900

    folderのinsertをauto incrementにする

--- src/main/scala/logic/folder/FolderLogicImpl.scala
+++ src/main/scala/logic/folder/FolderLogicImpl.scala
@@ -5,24 +5,21 @@ import model.{Folders => FolderTable, Folder => FolderModel}
 import scala.slick.driver.H2Driver.simple._
 
 class FolderLogicImpl(db: Database) extends FolderLogic {
-   override def insert(folder: FolderDto) = {
-     val folderQuery = TableQuery[FolderTable]
-     db.withTransaction { implicit session =>
-       folderQuery.insert(FolderModel(0, folder.owner, folder.name))
-       1 //TODO: 後でauto incrementにする。
-     }
-   }
+  private val folderQuery = TableQuery[FolderTable]
 
-   override def delete(folderId: Int) =
-     db.withTransaction{ implicit session =>
-       TableQuery[FolderTable].filter(_.id === folderId).delete
-     }
+  override def insert(folder: FolderDto): Int =
+    db.withTransaction { implicit session =>
+      (folderQuery returning folderQuery.map(_.id)) += FolderModel(-1, folder.owner, folder.name)
+    }
 
-   override def find(folderId: Int): Option[FolderDto] =
-     db.withTransaction { implicit session =>
-        TableQuery[FolderTable]
-         .filter(_.id === folderId)
-         .firstOption
-         .flatMap(x => Some(new FolderDto(x.id, x.owner, x.name)))
-     }
+  override def delete(folderId: Int) =
+    db.withTransaction { implicit session =>
+      folderQuery.filter(_.id === folderId).delete
+    }
+
+  override def find(folderId: Int): Option[FolderDto] =
+    db.withTransaction { implicit session =>
+      folderQuery.filter(_.id === folderId).firstOption
+        .flatMap(x => Some(new FolderDto(x.id, x.owner, x.name)))
+    }
 }
